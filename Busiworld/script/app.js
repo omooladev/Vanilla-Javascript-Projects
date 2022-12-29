b3ea8f7f60d22736908223d4b8e7d0e0eb96ae10
omodev <omooladev@gmail.com>
2022-12-29 10:37:31 +0100

    allowed user to be able to close navigation when they click on the body

--- Busiworld/script/app.js
+++ Busiworld/script/app.js
@@ -1,6 +1,23 @@
 const HiddenItems =
   document.querySelectorAll(".hidden");
 const Header = document.querySelector(".header");
+const mainBody = document.querySelector(".main");
+const footer = document.querySelector(".footer");
+const checkBox = document.querySelector(".menu");
+const mobileMenu = document.querySelector(
+  ".mobile-menu"
+);
+
+let isMenuActive;
+mobileMenu.addEventListener("click", () => {
+  isMenuActive = !checkBox.checked;
+});
+
+const closeNavigationHandler = () => {
+  if (isMenuActive) {
+    checkBox.checked = false;
+  }
+};
 
 const observer = new IntersectionObserver(
   (entries) => {
@@ -17,7 +34,14 @@ const observer = new IntersectionObserver(
 );
 
 HiddenItems.forEach((el) => observer.observe(el));
-
+mainBody.addEventListener(
+  "click",
+  closeNavigationHandler
+);
+footer.addEventListener(
+  "click",
+  closeNavigationHandler
+);
 window.addEventListener("scroll", () => {
   if (document.documentElement.scrollTop > 15) {
     Header.classList.add("sticky");
